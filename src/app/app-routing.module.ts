783fa74a2d2595c40149d68cea3917d1377f6e3c
Julio Cesar Melchor Pinto <julio.melchor@ciencias.unam.mx>
2020-06-24 15:47:38 -0500

    historical linechart start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,6 @@ import { WellcomeComponent } from './core/wellcome/wellcome.component';
 const routes: Routes = [
   { path: '', component: WellcomeComponent },
   {
-    path: 'module-two',
-    loadChildren: () =>
-      import('./modules/module-two/module-two.module').then(
-        (m) => m.ModuleTwoModule
-      ),
-  },
-  {
     path: 'statistics',
     loadChildren: () =>
       import('./modules/statistics/statistics.module').then(
--- src/app/modules/statistics/historical/historical.component.ts
+++ src/app/modules/statistics/historical/historical.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label, Color } from 'ng2-charts';
-import { PlotService } from '../services/plot.service';
 import { Oneday } from '../models/oneday';
 import { ApiService } from '../services/api.service';
 import { takeUntil } from 'rxjs/operators';
@@ -18,16 +17,12 @@ export class HistoricalComponent implements OnInit, OnDestroy {
   isLoading: boolean = true;
   public countryData: Oneday[];
   public dataline: number[];
-  public lineChartData: ChartDataSets[] = [{ data: [], label: 'Confirmed' }];
-  public lineChartLabels: Label[] = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-  ];
+  public dataConfirm: number[];
+  countryName: string;
+
+  public datalabel: Label[];
+  public lineChartData: ChartDataSets[] = [{ data: [], label: '' }];
+  public lineChartLabels: Label[] = [];
   public lineChartOptions: ChartOptions = {
     responsive: true,
   };
@@ -48,15 +43,27 @@ export class HistoricalComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.countryName = 'mexico';
     this._apiService
-      .sendGetRequest('summary')
+      .sendGetRequest('total/dayone/country/' + this.countryName)
       .pipe(takeUntil(this.destroy$))
       .subscribe((res: HttpResponse<any>) => {
-        this.countryData = res.body;
-        this.countryData.forEach((element) => {
-          console.log(element.confirmed.valueOf);
-          this.dataline.push(element.confirmed);
-        });
+        const myObj = res.body;
+        var confirmedData = myObj.map(({ Confirmed }) => Confirmed);
+        var activeData = myObj.map(({ Active }) => Active);
+        var recoveredData = myObj.map(({ Recovered }) => Recovered);
+        var deathsData = myObj.map(({ Deaths }) => Deaths);
+
+        var selectedLabel = myObj.map(({ Date }) => Date);
+        var charDataSet: ChartDataSets[] = [
+          { data: confirmedData, label: 'Confirmed' },
+          { data: activeData, label: 'Active' },
+          { data: recoveredData, label: 'Recovered' },
+          { data: deathsData, label: 'Deaths' },
+        ];
+        this.lineChartData = charDataSet;
+        this.lineChartLabels = selectedLabel;
+        this.isLoading = !this.isLoading;
       });
   }
 }
