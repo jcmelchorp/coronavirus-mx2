0ceba02ee893263c9cd558b2722ca3c5384fb860
Julio Melchor <julio.melchor@ciencias.unam.mx>
2020-06-28 16:28:26 -0500

    SIR model with reactive form works!

--- src/app/modules/modeling/s-i-r/s-i-r.component.ts
+++ src/app/modules/modeling/s-i-r/s-i-r.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { KatexOptions } from 'ng-katex';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label, Color } from 'ng2-charts';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 export interface Result {
   suc?: number;
@@ -17,19 +17,10 @@ export interface Result {
   styleUrls: ['./s-i-r.component.scss'],
 })
 export class SIRComponent implements OnInit {
-  public sucept: number;
-  public infect: number;
-  public r: number;
-  public steps: number;
-  public t: number;
-  public dsdt: number;
-  public dinfectdt: number;
-  public drdt: number;
-  public result: Result = { t: 0, suc: 0, inf: 0, rec: 0 };
   public results: Result[] = [];
   public sirData: ChartDataSets[] = [{ data: [], label: '' }];
   public lineChartLabels: Label[] = [];
-  public lineChartOptions: ChartOptions = { responsive: true };
+  // Plot parameters
   public lineChartColors: Color[] = [
     {
       borderColor: 'rgba(0,120,0,1)',
@@ -44,140 +35,145 @@ export class SIRComponent implements OnInit {
       backgroundColor: 'rgba(230,160,0,0.5)',
     },
   ];
-  public lineChartLegend = true;
-  public lineChartType: ChartType = 'line';
-  public lineChartPlugins = [];
-
-  constructor(private formBuilder: FormBuilder) {}
-
-  sirForm = this.formBuilder.group({
-    totalPopulation: [
+  // Form
+  public sirForm = this.formBuilder.group({
+    totalPop: [
       null,
       Validators.compose([
         Validators.required,
         Validators.minLength(1),
         Validators.maxLength(8),
-      ]),
-    ],
-    initialInfected: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(1),
-        Validators.maxLength(8),
-      ]),
-    ],
-    initialSucept: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(1),
-        Validators.maxLength(8),
-      ]),
-    ],
-    totalTime: [
-      null,
-      Validators.compose([
-        Validators.required,
-        Validators.minLength(1),
-        Validators.maxLength(5),
       ]),
     ],
     beta: [
       null,
       Validators.compose([
         Validators.required,
-        Validators.min(-2),
-        Validators.max(2),
+        Validators.min(0),
+        Validators.max(1),
       ]),
     ],
     gamma: [
       null,
       Validators.compose([
         Validators.required,
-        Validators.min(-2),
-        Validators.max(2),
+        Validators.min(0),
+        Validators.max(1),
       ]),
     ],
+    delta_t: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.min(0),
+        Validators.max(1),
+      ]),
+    ],
+    totalTime: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.min(0),
+        Validators.max(5000),
+      ]),
+    ],
+    i0: [null, Validators.compose([Validators.required, Validators.min(1)])],
+    r0: [null, Validators.compose([Validators.required, Validators.min(0)])],
   });
+  constructor(private formBuilder: FormBuilder) {}
 
-  hasUnitNumber = false;
-
-  onSubmit() {
-    this.results = this.calculate();
-    var time = this.results.map(({ t }) => t);
-    var suceptible = this.results.map(({ suc }) => suc);
-    var infected = this.results.map(({ inf }) => inf);
-    var recovered = this.results.map(({ rec }) => rec);
-    var outDataSet: ChartDataSets[] = [
-      { data: suceptible, label: 'Suceptibles' },
-      { data: infected, label: 'Infectados' },
-      { data: recovered, label: 'Recuperados' },
-    ];
-    var days: string[] = [''];
-    for (var i in time) {
-      days[i] = time[i].toString();
-    }
-    this.sirData = outDataSet;
-    this.lineChartLabels = days;
+  ngOnInit(): void {
+    this.sirForm.setValue({
+      totalPop: 1000,
+      beta: 0.2,
+      gamma: 0.1,
+      delta_t: 0.25,
+      totalTime: 80,
+      i0: 1,
+      r0: 0,
+    });
+    this.plotData(this.sirForm.value);
   }
 
-  calculate(): Result[] {
+  onSubmit() {
+    this.plotData(this.sirForm.value);
+  }
+
+  plotData(formData: any) {
+    var N = formData.totalPop;
+    var beta = formData.beta;
+    var gamma = formData.gamma;
+    var dt = formData.delta_t;
+    var totalTime = formData.totalTime;
+    var i0 = formData.i0;
+    var r0 = formData.r0;
+    this.results = this.calculate(N, beta, gamma, dt, totalTime, i0, r0);
+    this.sirData = this.createSeries(this.results).outDataSet;
+    this.lineChartLabels = this.createSeries(this.results).days;
+  }
+
+  calculate(
+    N: number,
+    beta: number,
+    gamma: number,
+    dt: number,
+    totalTime: number,
+    initInfected: number,
+    initRecover: number
+  ): Result[] {
+    var sucept: number;
+    var infect: number;
+    var r: number;
+    var steps: number;
+    var t: number;
+    var dsdt: number;
+    var dinfectdt: number;
+    var drdt: number;
     var output: Result[] = [];
-    const N = 1000; // total population
-    const beta = 0.2;
-    const gamma = 0.1;
-    const dt = 0.25; //days
-    const totalTime = 120; //days
-    this.steps = totalTime * (1 / dt);
-    for (var i = 0; i <= this.steps; i++) {
-      this.t = dt * i;
+    steps = totalTime * (1 / dt);
+    for (var i = 0; i <= steps; i++) {
+      t = dt * i;
 
       if (i == 0) {
-        this.sucept = 999;
-        this.infect = 1;
-        this.r = 0;
+        sucept = N - initInfected;
+        infect = initInfected;
+        r = initRecover;
       } else {
-        this.sucept = this.sucept + (dt / 2) * this.dsdt;
-        this.infect = this.infect + (dt / 2) * this.dinfectdt;
-        this.r = this.r + (dt / 2) * this.drdt;
+        sucept = sucept + (dt / 2) * dsdt;
+        infect = infect + (dt / 2) * dinfectdt;
+        r = r + (dt / 2) * drdt;
       }
       /* -- Euler Step -- */
       // iResult[] = [];
-      this.dsdt = (-beta * this.sucept * this.infect) / N;
-      this.dinfectdt =
-        (beta * this.sucept * this.infect) / N - gamma * this.infect;
-      this.drdt = gamma * this.infect;
+      dsdt = (-beta * sucept * infect) / N;
+      dinfectdt = (beta * sucept * infect) / N - gamma * infect;
+      drdt = gamma * infect;
       // i + 1
-      this.sucept = this.sucept + dt * this.dsdt;
-      this.infect = this.infect + dt * this.dinfectdt;
-      this.r = this.r + dt * this.drdt;
+      sucept = sucept + dt * dsdt;
+      infect = infect + dt * dinfectdt;
+      r = r + dt * drdt;
       /* -- end of Euler Step -- */
 
       /* -- Fordward diferentiation -- */
       // i + 1
-      this.dsdt = (-beta * this.sucept * this.infect) / N;
-      this.dinfectdt = -this.dsdt - gamma * this.infect;
-      this.drdt = gamma * this.infect;
-      /* this.result.t = this.t;
-      this.result.suc = this.sucept;
-      this.result.inf = this.infect;
-      this.result.rec = this.r; */
+      dsdt = (-beta * sucept * infect) / N;
+      dinfectdt = -dsdt - gamma * infect;
+      drdt = gamma * infect;
+      // preparing results
       output.push({ t: 0, suc: 0, inf: 0, rec: 0 });
-      output[i].t = this.t;
-      output[i].suc = this.sucept;
-      output[i].inf = this.infect;
-      output[i].rec = this.r;
+      output[i].t = t;
+      output[i].suc = sucept;
+      output[i].inf = infect;
+      output[i].rec = r;
     }
     return output;
   }
 
-  ngOnInit(): void {
-    this.results = this.calculate();
-    var time = this.results.map(({ t }) => t);
-    var suceptible = this.results.map(({ suc }) => suc);
-    var infected = this.results.map(({ inf }) => inf);
-    var recovered = this.results.map(({ rec }) => rec);
+  createSeries(resultArr: Result[]) {
+    var time = resultArr.map(({ t }) => t);
+    var suceptible = resultArr.map(({ suc }) => suc);
+    var infected = resultArr.map(({ inf }) => inf);
+    var recovered = resultArr.map(({ rec }) => rec);
     var outDataSet: ChartDataSets[] = [
       { data: suceptible, label: 'Suceptibles' },
       { data: infected, label: 'Infectados' },
@@ -187,7 +183,6 @@ export class SIRComponent implements OnInit {
     for (var i in time) {
       days[i] = time[i].toString();
     }
-    this.sirData = outDataSet;
-    this.lineChartLabels = days;
+    return { outDataSet, days };
   }
 }
